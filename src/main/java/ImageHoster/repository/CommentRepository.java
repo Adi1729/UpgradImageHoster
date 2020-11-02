f6aaade2bdcb5297d80ae16b9e381b99a5bd187d
adi1729 <adiyadav.1729@gmail.com>
2020-11-02 22:51:56 +0530

    initial

---
+++ src/main/java/ImageHoster/repository/CommentRepository.java
@@ -0,0 +1,54 @@
+package ImageHoster.repository;
+
+import ImageHoster.model.Comment;
+import ImageHoster.model.Image;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.*;
+import java.util.List;
+
+//The annotation is a special type of @Component annotation which describes that the class defines a data repository
+@Repository
+public class CommentRepository {
+
+    //Get an instance of EntityManagerFactory from persistence unit with name as 'imageHoster'
+    @PersistenceUnit(unitName = "imageHoster")
+    private EntityManagerFactory emf;
+
+
+    //The method receives the Image object to be persisted in the database
+    //Creates an instance of EntityManager
+    //Starts a transaction
+    //The transaction is committed if it is successful
+    //The transaction is rolled back in case of unsuccessful transaction
+    public Comment addComment(Comment addComment) {
+
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction transaction = em.getTransaction();
+
+        try {
+            transaction.begin();
+            em.persist(addComment);
+            transaction.commit();
+        } catch (Exception e) {
+            transaction.rollback();
+        }
+        return addComment;
+
+    }
+
+    //The method creates an instance of EntityManager
+    //Executes JPQL query to fetch the image from the database with corresponding id
+    //Returns the image fetched from the database
+
+    public List<Comment> getAllComment() {
+        EntityManager em = emf.createEntityManager();
+        TypedQuery<Comment> query = em.createQuery("SELECT i from Comment i", Comment.class);
+        List<Comment> resultList = query.getResultList();
+        return resultList;
+
+    }
+
+
+
+}
---
+++ src/main/java/ImageHoster/service/CommentService.java
@@ -0,0 +1,27 @@
+package ImageHoster.service;
+
+import ImageHoster.model.Comment;
+import ImageHoster.model.Image;
+import ImageHoster.repository.CommentRepository;
+import ImageHoster.repository.ImageRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CommentService {
+
+    @Autowired
+    private CommentRepository commentRepository;
+    //The method calls the createImage() method in the Repository and passes the image to be persisted in the database
+    public void addComment(Comment comment) {
+        commentRepository.addComment(comment);
+    }
+
+    public List<Comment> getAllComment() {
+        return commentRepository.getAllComment();
+    }
+
+
+}
